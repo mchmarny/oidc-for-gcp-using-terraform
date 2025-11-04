246ba6a80eb4534d9134222c0222e96ce8718725
Mark Chmarny <mchmarny@nvidia.com>
2025-11-04 06:18:37 -0800

    chore: upgrade tf modules

--- federation.tf
+++ federation.tf
@@ -11,7 +11,6 @@ locals {
 
 # GCR registry 
 resource "google_artifact_registry_repository" "registry" {
-  provider = google-beta
   project = var.project_id
 
   description = "Trager artifacts registry"
@@ -28,7 +27,6 @@ resource "google_service_account" "github_actions_user" {
 
 # Role binding to allow publisher to publish images
 resource "google_artifact_registry_repository_iam_member" "github_actions_user_storage_role_binding" {
-  provider   = google-beta
   project    = var.project_id
   location   = var.registry_location
   repository = google_artifact_registry_repository.registry.name
@@ -38,13 +36,11 @@ resource "google_artifact_registry_repository_iam_member" "github_actions_user_s
 
 # Identiy pool for GitHub action based identity's access to Google Cloud resources
 resource "google_iam_workload_identity_pool" "github_pool" {
-  provider                  = google-beta
   workload_identity_pool_id = "github-pool"
 }
 
 # Configuration for GitHub identiy provider
 resource "google_iam_workload_identity_pool_provider" "github_provider" {
-  provider                           = google-beta
   workload_identity_pool_id          = google_iam_workload_identity_pool.github_pool.workload_identity_pool_id
   workload_identity_pool_provider_id = "github-provider"
   attribute_mapping = {
@@ -53,6 +49,7 @@ resource "google_iam_workload_identity_pool_provider" "github_provider" {
     "attribute.actor"      = "assertion.actor"
     "attribute.repository" = "assertion.repository"
   }
+  attribute_condition = "assertion.repository == '${var.git_repo}'"
   oidc {
     issuer_uri        = "https://token.actions.githubusercontent.com"
     allowed_audiences = []
@@ -61,7 +58,6 @@ resource "google_iam_workload_identity_pool_provider" "github_provider" {
 
 # IAM policy bindings to the service account resources created by GitHub identify
 resource "google_service_account_iam_member" "pool_impersonation" {
-  provider           = google-beta
   service_account_id = google_service_account.github_actions_user.id
   role               = "roles/iam.workloadIdentityUser"
   member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/${var.git_repo}"
